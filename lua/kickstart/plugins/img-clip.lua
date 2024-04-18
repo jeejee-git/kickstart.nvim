b9e8710e1e20b834c6af9baca998cad4e09c0b6f
jeejee <dulanjana.alwis@icloud.com>
2024-04-18 16:36:14 +0700

    mods

--- lua/kickstart/plugins/img-clip.lua
+++ lua/kickstart/plugins/img-clip.lua
@@ -1,23 +1,22 @@
 -- Paste image from clipboard
 return {
-  "HakonHarnes/img-clip.nvim",
-  event = "VeryLazy",
-  opts = {
-    -- add options here
-    -- or leave it empty to use the default settings
+	"HakonHarnes/img-clip.nvim",
+	event = "VeryLazy",
+	opts = {
+		-- add options here
+		-- or leave it empty to use the default settings
 
-    -- filetype specific options
-    filetypes = {
-      markdown = {
-        url_encode_path = true, ---@type boolean
-        template = "![$CURSOR]($FILE_PATH)", ---@type string
-        prompt_for_file_name = false,
-      },
-    },
-
-  },
-  keys = {
-    -- suggested keymap
-    { "<leader>p", "<cmd>PasteImage<cr>", desc = "Paste image from system clipboard" },
-  },
+		-- filetype specific options
+		filetypes = {
+			markdown = {
+				url_encode_path = true, ---@type boolean
+				template = "![$CURSOR]($FILE_PATH)", ---@type string
+				prompt_for_file_name = false,
+			},
+		},
+	},
+	keys = {
+		-- suggested keymap
+		{ "<leader>p", "<cmd>PasteImage<cr>", desc = "Paste image from system clipboard" },
+	},
 }
--- lua/kickstart/plugins/markdown-preview.lua
+++ lua/kickstart/plugins/markdown-preview.lua
@@ -1,11 +1,12 @@
 -- Preview markdown
 return {
-  "iamcco/markdown-preview.nvim",
-  cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-  ft = { "markdown" },
-  build = function() vim.fn["mkdp#util#install"]() end,
-  init = function()
-    vim.g.mkdp_theme = 'light'
-  end
+	"iamcco/markdown-preview.nvim",
+	cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+	ft = { "markdown" },
+	build = function()
+		vim.fn["mkdp#util#install"]()
+	end,
+	init = function()
+		vim.g.mkdp_theme = "light"
+	end,
 }
-
